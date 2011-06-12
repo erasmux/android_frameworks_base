b88f65d7958f3a9d2d2f4397eba6bdcc91c5f074
Eran Mizrahi <eranmizra@gmail.com>
2011-06-12 03:05:52 +0300

    -Fixes issue 3573 (http://code.google.com/p/cyanogenmod/issues/detail?id=3573) with RTL characters wrapping down to the next line when there's enough space on the line itself.
    -Fixes issue with RTL characters overflowing outside the allowable margin for some layouts
    
    -Optimizes measurement of layouts and text by only processing Bidi if there are any Arabic characters available
    as opposed to processing Bidi if there are any RTL character available. (This is because Arabic is the only RTL
    language where the width of characters changes due to reshaping. Other RTL languages do not require reshaping
    and hence do not require this extra processing.)
    
    ***Special thanks to Eran Mizrahi***
    
    Change-Id: I8ca00c564c2fede51e246222eeb0e3122b0b6fec
    
    Conflicts:
    
    	core/java/android/text/TextUtils.java

--- core/java/android/text/TextUtils.java
+++ core/java/android/text/TextUtils.java
@@ -18,11 +18,9 @@ package android.text;
 
 import com.android.internal.R;
 
-import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.os.Parcel;
 import android.os.Parcelable;
-import android.text.method.TextKeyListener.Capitalize;
 import android.text.style.AbsoluteSizeSpan;
 import android.text.style.AlignmentSpan;
 import android.text.style.BackgroundColorSpan;
@@ -1725,6 +1723,17 @@ public class TextUtils {
     }
 
     /**
+     * @hide
+     */
+    private static boolean isArabicCharacter(char c) {
+        //range of Arabic characters per unicode specification
+        return (c >= 0x0600 && c <= 0x06FF) ||
+               (c >= 0x0750 && c <= 0x077F) ||
+               (c >= 0xFB50 && c <= 0xFDFF) ||
+               (c >= 0xFE70 && c <= 0xFEFE)    ;
+    }
+
+    /**
      * function to check if text range has RTL characters.
      */
     private static boolean hasRTLCharacters(final char[] text, int start, int end) {
@@ -1734,6 +1743,40 @@ public class TextUtils {
         //go through all characters
         for (int i = start; i < end; i++) {
             if (isRTLCharacter(text[i]))
+                return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * function to check if text range has Arabic characters.
+     * @hide
+     */
+    private static boolean hasArabicCharacters(final char[] text, int start, int end) {
+        if (text == null)
+            return false;
+
+        //go through all characters
+        for (int i = start; i < end; i++) {
+            if (isArabicCharacter(text[i]))
+                return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * function to check if text range has Arabic characters.
+     * @hide
+     */
+    public static boolean hasArabicCharacters(String text, int start, int end) {
+        if (text == null)
+            return false;
+
+        //go through all characters
+        for (int i = start; i < end; i++) {
+            if (isArabicCharacter(text.charAt(i)))
                 return true;
         }
 
@@ -1766,12 +1809,32 @@ public class TextUtils {
     }
 
     /**
+     * function to reshape the given text
+     * @param src
+     * @return String
+     * @hide
+     */
+    public static String reshapeArabic(final String src) {
+        return src == null ? null : TextUtils.reshapeArabic(src, 0, src.length());
+    }
+
+    /**
      * function to process bidi the given text
      * @param src
      * @return char[]
      */
     public static char[] processBidi(final char[] src) {
         return src == null ? null : TextUtils.processBidi(src, 0, src.length);
+    }
+
+    /**
+     * function to reshape the given text
+     * @param src
+     * @return char[]
+     * @hide
+     */
+    public static char[] reshapeArabic(final char[] src) {
+        return src == null ? null : TextUtils.reshapeArabicChars(src, 0, src.length);
     }
 
     /**
@@ -1786,6 +1849,19 @@ public class TextUtils {
     }
 
     /**
+     * function to reshape the given text
+     * @param src
+     * @param begin
+     * @param end
+     * @return String
+     * @hide
+     */
+    public static String reshapeArabic(final String src, int start, int end) {
+        return src != null && hasArabicCharacters(src, start, end) ? String.valueOf(TextUtils.reshapeArabicChars(src.toCharArray(), start, end)) : src;
+    }
+
+    /**
+     * function to process bidi on the given text
      * @author: Eyad Aboulouz
      * function to process bidi on the given text
      * @param src
@@ -1794,7 +1870,20 @@ public class TextUtils {
      * @return char[]
      */
     public static char[] processBidi(final char[] src, int start, int end) {
-        return src != null && hasRTLCharacters(src, start, end) ? processBidiChars(src, start, end) : src;
+        return src != null && hasRTLCharacters(src, start, end) ? TextUtils.processBidiChars(src, start, end) : src;
+    }
+
+    /**
+     * function to reshape the given text
+     * @author: Eyad Aboulouz
+     * @param src
+     * @param start
+     * @param end
+     * @return char[]
+     * @hide
+     */
+    public static char[] reshapeArabic(final char[] src, int start, int end) {
+        return src != null && hasArabicCharacters(src, start, end) ? TextUtils.reshapeArabicChars(src, start, end) : src;
     }
 
     /**
@@ -1813,6 +1902,36 @@ public class TextUtils {
             char[] ret = src.clone();
 
             int outputSize = AndroidBidi.reorderAndReshapeBidiText(ret, outputTxt, start, end-start);
+
+            if (outputSize != (end-start))
+                throw new Exception ("Error Processing Bidi Reordering And Reshaping");
+
+            System.arraycopy(outputTxt, 0, ret, start, end-start);
+
+            return (ret);
+
+        } catch (Exception e) {
+
+            return (src);
+        }
+    }
+
+    /**
+     * function to reshape arabic text
+     * @author: Eyad Aboulouz
+     * @param src
+     * @param start
+     * @param end
+     * @return char[]
+     * @hide
+     */
+    private static char[] reshapeArabicChars(final char[] src, int start, int end) {
+
+        try {
+            char[] outputTxt = new char[end-start];
+            char[] ret = src.clone();
+
+            int outputSize = AndroidBidi.reshapeReversedArabicText(ret, outputTxt, start, end-start);
 
             if (outputSize != (end-start))
                 throw new Exception ("Error Processing Bidi Reordering And Reshaping");
